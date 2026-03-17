380f7c045b28b33e1e7c1617cec52f03d671cf95
Sonnenbergenfluganschluss <zavtra2005@mail.ru>
2026-03-17 19:04:23 +0300

    sdf

--- myauthapp/application/for_views/calculate/methods/open_points.py
+++ myauthapp/application/for_views/calculate/methods/open_points.py
@@ -1,4 +1,4 @@
-from ...constants import open_sky_points, ustya
+from ...constants import open_sky_points, ustya, najafa, color_dict
 import datetime
 
 def get_additional_points(day_iero, solartime):
@@ -24,6 +24,30 @@ def get_additional_points(day_iero, solartime):
         if v[0] <= next_time.time() < v[1]:
             next_point = k.capitalize()
             time_slot = [v[0].strftime('%H:%M'), v[1].strftime('%H:%M')]    
+
+    # Поиск точек Najafa
+    for k, v in najafa.items():
+        if k==day_iero[0]:
+            current_day = v
+            if current_time < current_day['change_day']:
+                points_of_space = current_day['points_of_space'][0]
+            else:
+                points_of_space = current_day['points_of_space'][1]
+            
+            points_of_time = current_day['points_of_time']
+            for k, v in points_of_time.items():
+                if v[0] <= current_time < v[1]:
+                    point_of_time = k.capitalize()
+
+            last_28_minutes = current_day['last_28_minutes']
+            for k, v in last_28_minutes.items():
+                if v[0] <= current_time < v[1]:
+                    point_of_last_28_minutes = k.capitalize()
+                    last_datetime = datetime.datetime(2000, 1, 1, int(v[1].hour), int(v[1].minute))
+                    last_time = last_datetime - datetime.timedelta(minutes=27, seconds=0)
+                    last_28_minutes_slot = (last_time.strftime('%H:%M'), v[1].strftime('%H:%M'))
+
+
             
     result = f'''
             <div class=container>
@@ -49,7 +73,17 @@ def get_additional_points(day_iero, solartime):
                         ставится билатерально. <br>
                         3. <span style='font-weight: bold;'>Перед извлечением игл</span> стимулируем точку-устье канала согласно времени: <br>
                         c {current_slot[0]} до {current_slot[1]} - точка <span style='color:#588ed4; font-weight: bold;'>{cur_point}</span>. 
-                        <br> {day_iero}
+                    </div>
+                </div>
+            </div>
+            <div class=container>
+                <div style='font-weight: bold;'>Метод Na ja fa:</div>
+                <div class=container style=padding:20px;>
+                    <div> 
+                        Небесный ствол дня сегодня: <span style='color: {color_dict[day_iero[0]]}; font-weight: bold;'>{day_iero[0]}</span> <br>
+                        Точки времени: <span style='color:#588ed4; font-weight: bold;'>{point_of_time}</span>. <br>
+                        Точки пространства: <span style='color:#588ed4; font-weight: bold;'>{points_of_space}</span>. <br>
+                        Точки последних 28 минут стража <span style='color:#588ed4; font-weight: bold;'>{point_of_last_28_minutes}</span>, активны с {last_28_minutes_slot[0]} по {last_28_minutes_slot[1]}: . <br>
                     </div>
                 </div>
             </div>
